cb5addcf8e9ff771dfdd9f68da5d389a95609e57
Ivy Wong <ivy.wong022@gmail.com>
2015-05-08 11:31:25 -0400

    Add basic profile page

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def main():
     username = "wally"
     return render_template("main.html",username=username)
 
+@app.route("/user/<username>")
+def profile(username=None):
+    return render_template("profile.html",username=username)
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
