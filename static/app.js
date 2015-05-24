5a509ae6f567a47d43b291e5e77f438bab3cc316
Applesauce136 <harrison.chiu.136@gmail.com>
2015-05-24 11:38:48 -0400

    basic ability statsgit status

--- static/app.js
+++ static/app.js
@@ -5,51 +5,66 @@ var App = new Marionette.Application();
 
 var Stat = Backbone.Model.extend({
     defaults: {
+        id: "undefined",
         name: "statName",
-        value: 0
+        value: 0,
+        mutable: true
     }
 });
 
 var Character = Backbone.Collection.extend({
-    model: Stat
+    model: Stat,
+    initialize: function () {
+        this.makeAbility("strength");
+        this.makeAbility("dexterity");
+        this.makeAbility("constitution");
+        this.makeAbility("intelligence");
+        this.makeAbility("wisdom");
+        this.makeAbility("charisma");
+    },
+    makeAbility: function (statName) {
+        this.add(new Stat({
+            id: statName,
+            name: statName,
+            value: 10,
+            mutable: true
+        }));
+        console.log(this.get(statName));
+        this.add(new Stat({
+            id: statName + " mod.",
+            name: statName + " mod.",
+            value: 0,
+            mutable: false
+        }));
+        this.on("change", function () {
+                this.get(statName + " mod.")
+                .set("value",
+                     Math.floor((this.get(statName).get("value") - 10)
+                                / 2))});
+}
 });
-
-// var Character = Backbone.Model.extend({
-//     model: StatSet
-//     // stats: {
-
-//     //     },
-//     // actions: {
-
-//     //     },
-//     // defaults: {
-//     //     "stats": {
-//     //         "name": "Redma",
-//     //         "abilities": {
-//     //             "str": 9,
-//     //             "dex": 11,
-//     //             "con": 15,
-//     //             "int": 15,
-//     //             "wis": 7,
-//     //             "cha": 16
-//     //         },
-//     //         "modifiers": {
-//     //             "str": -1nn
-//     //         }
-//     //     },
-//     //     "actions": {
-//     //         "attack": function(roll) {
-//     //             return roll + this.get("stats")["abilities"]["str"];
-//     //         }
-//     //     }
-//     // }
-// });
 
 // VIEW STUFF
 
 var StatView = Marionette.ItemView.extend({
     template: "#stat-template",
-    model: Stat
+    tagName: "tr",
+    model: Stat,
+    initialize: function () {
+        this.listenTo(this.model, "change", this.render);
+    },
+    ui: {
+        value: "#value",
+        update: "#update"
+    },
+    events: {
+        "click @ui.update": function () {
+            var newval = this.ui.value.val();
+            if (!isNaN(parseInt(newval))) {
+                this.model.set("value", parseInt(newval));
+            }
+        }
+    }
 });
 
 var CharacterView = Marionette.CompositeView.extend({
@@ -60,23 +75,13 @@ var CharacterView = Marionette.CompositeView.extend({
 });
 
 App.addRegions({
-    statRegion: "#stat-region",
     characterRegion: "#character-region"
 });
 
 App.on("start", function() {
     console.log("App started");
-
-    var stat = new Stat(
-        {name: "strength", value: 10}
-    );
-    var statView = new StatView({model:stat});
-    App.statRegion.show(statView);
     
-    var character = new Character([
-        new Stat({name: "dexterity", value: 11}),
-        new Stat({name: "intelligence", value: 12}),
-    ]);
+    var character = new Character();
     
     var characterView = new CharacterView({collection:character});
     App.characterRegion.show(characterView);
